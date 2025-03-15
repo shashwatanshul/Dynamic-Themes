6a6fef211175c00a4c5a796b4c899e782a0336c7
Shashwat Singh <shashwatanshul@gmail.com>
2025-03-15 13:29:23 +0530

    inr icon
    inr icon

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -18,11 +18,16 @@ function ProductCard({ product }) {
       <div className="card-body">
         {/* PRODUCT INFO */}
         <h2 className="card-title text-lg font-semibold">{product.name}</h2>
-        <p className="text-2xl font-bold text-primary">${Number(product.price).toFixed(2)}</p>
+        <p className="text-2xl font-bold text-primary">
+          &#8377{Number(product.price).toFixed(2)}
+        </p>
 
         {/* CARD ACTIONS */}
         <div className="card-actions justify-end mt-4">
-          <Link to={`/product/${product.id}`} className="btn btn-sm btn-info btn-outline">
+          <Link
+            to={`/product/${product.id}`}
+            className="btn btn-sm btn-info btn-outline"
+          >
             <EditIcon className="size-4" />
           </Link>
 
