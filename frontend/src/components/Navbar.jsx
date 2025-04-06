2cbb0991df354fd5a1123bd5294cc6c30f1edd6a
Shashwat Singh <shashwatanshul@gmail.com>
2025-04-06 12:24:53 +0530

    updated 6th april 2025
    updated 6th april 2025

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -22,6 +22,7 @@ function Navbar() {
                   className="font-semibold font-mono tracking-widest text-2xl 
                     bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary"
                 >
+                  {" "}
                   THEMES STORE
                 </span>
               </div>
